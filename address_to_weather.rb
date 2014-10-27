ac4590a47cc7b984009cd105c54ce524f2e459a4
dlondon4 <dlondon4@gmail.com>
2014-10-27 12:36:56 -0500

    Pull in parts of other programs

--- address_to_weather.rb
+++ address_to_weather.rb
@@ -12,8 +12,20 @@ puts "Let's get the weather forecast for your address."
 puts "What is the address you would like to know the weather for?"
 the_address = gets.chomp
 url_safe_address = URI.encode(the_address)
+url_of_data_we_want = "http://maps.googleapis.com/maps/api/geocode/json?address=" + url_safe_address
+raw_data = open(url_of_data_we_want).read
+parsed_data = JSON.parse(raw_data)
 
-# Your code goes below.
+the_latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+the_longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+url = "https://api.forecast.io/forecast/5fee92a639af2acdf2b460fed7a521ac/#{the_latitude},#{the_longitude}"
+raw_data = open(url).read
+parsed_data = JSON.parse(raw_data)
+
+the_temperature = parsed_data["currently"]["temperature"] #current temperature
+the_hour_outlook = parsed_data["hourly"]["summary"] #hourly summary
+the_day_outlook = parsed_data["daily"]["summary"] #daily summary
 
 # Ultimately, we want the following line to work when uncommented:
 
